9ba2fe26cec875bd9f821d60aaab5809eaa63b1b
rishavmishra2051 <114403095+rishavmishra2051@users.noreply.github.com>
2023-06-28 13:06:12 +0530

    Create P01_Rectangle.js

---
+++ P01_Rectangle.js
@@ -0,0 +1,5 @@
+var l=parseInt(prompt("Length: "));
+var b=parseInt(prompt("Breadth: "));
+var area=l*b;
+var peri=2*(l+b);
+console.log(`Area of Rectangle is ${area} and Perimeter of Rectangle is ${peri}`);
