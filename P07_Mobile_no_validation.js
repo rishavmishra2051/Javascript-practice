39481f32bb156b7785aa7b8ae5dd5b4bf869843f
rishavmishra2051 <114403095+rishavmishra2051@users.noreply.github.com>
2023-06-30 00:28:30 +0530

    Create P07_Mobile_no_validation.js

---
+++ P07_Mobile_no_validation.js
@@ -0,0 +1,10 @@
+function mobile_no(n) {
+    var t="";
+    t=t+n;
+    if(t.length != 10) {
+      alert('10 digit mobile number is required');
+    }
+    else{
+        alert('10 digit mobile number is entered');
+    }
+}
