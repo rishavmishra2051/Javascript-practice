ee96f0f87d4bd00144cc2741d2f9b28e1525490c
rishavmishra2051 <114403095+rishavmishra2051@users.noreply.github.com>
2023-07-01 12:43:10 +0530

    Update and rename Assignment-4 to Assignment_4.js

---
+++ Assignment_4.js
@@ -0,0 +1,11 @@
+/*Find Occurance of a target value in given array;
+arr= [19, 22, 18, 19, 16, 18, 19, 21, 24];
+target = 19;*/
+//code
+var arr=arr= [19, 22, 18, 19, 16, 18, 19, 21, 24];
+arr.filter(e=>e==19);
+arr.filter(e=>e==19).length;
+
+/*Reverse a given string using reverse() method 
+input = "BrainMentors"
+output = "srotnemniarB"*/
